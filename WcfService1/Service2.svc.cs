598434237b631f211f51060c55b183d562a3a001
Jos� Alejandro Maselli Garc�a <jose.maselli@unealum.local>
2023-11-13 17:12:40 +0100

    first commit

---
+++ WcfService1/Service2.svc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service2" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service2.svc o Service2.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Service2 : IService2
+    {
+        public void DoWork()
+        {
+            Console.WriteLine("hola");
+        }
+
+        public void miMetodo() { }
+    }
+}
